8333debd1f03a5f6399f7b4b8b6e9291edf62e3a
Maulidan Nashuha <maulidannashuha@gmail.com>
2022-12-27 17:47:36 +0700

    set prefix sso on route

--- src/routes.php
+++ src/routes.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use DiskominfotikBandaAceh\SSOBandaAcehPHP\Http\Controllers\SSOController;
 use DiskominfotikBandaAceh\SSOBandaAcehPHP\Http\Controllers\LoginController;
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => ['web', 'prefix' => 'sso', 'as' => 'sso.']], function () {
     Route::get('/login', [LoginController::class, 'login'])->name('login');
 
-    Route::group(['middleware' => ['guest'], 'prefix'=>'auth', 'as'=>'sso.'], function () {
+    Route::group(['middleware' => ['guest'], 'prefix'=>'auth'], function () {
         Route::get('/redirect', [SSOController::class, 'redirect'])->name('redirect');
         Route::get('/callback', [SSOController::class, 'callback'])->name('callback');
         Route::get('/logout', [SSOController::class, 'logout'])->name('logout');
